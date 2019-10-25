0cced43643ed466fae31db119a3a17062dab1227
Joe Johnston <jjoh1051@ford.com>
2019-10-25 15:49:47 -0400

    built the app to add it to a github page

---
+++ docs/precache-manifest.d042bb95750e9b07f992a6e2a2a7bb7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03ddffbe3f10eed551d9cdaad05c9147",
+    "url": "/JArthurJohnston/mafia.io/index.html"
+  },
+  {
+    "revision": "efce6dc6c5f82b25709b",
+    "url": "/JArthurJohnston/mafia.io/static/css/main.216f370b.chunk.css"
+  },
+  {
+    "revision": "52ec7ebc78d0a5e5dbfe",
+    "url": "/JArthurJohnston/mafia.io/static/js/2.c291a851.chunk.js"
+  },
+  {
+    "revision": "efce6dc6c5f82b25709b",
+    "url": "/JArthurJohnston/mafia.io/static/js/main.a3eeee92.chunk.js"
+  },
+  {
+    "revision": "83a15d97cc6b0c1b2fb3",
+    "url": "/JArthurJohnston/mafia.io/static/js/runtime-main.658fdc91.js"
+  }
+]);
