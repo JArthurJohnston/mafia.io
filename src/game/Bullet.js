bceb4b1108b17ed88dbacd4ba25a2b585d1d7c89
Joe Johnston <joe.a.johnston@gmail.com>
2019-11-09 16:25:53 -0500

    fixed the bullet and explosion positioning so theyre parented to the level, and move with it

--- src/game/Bullet.js
+++ src/game/Bullet.js
@@ -1,8 +1,7 @@
 import { GameObject } from "./engine/GameObject";
-import { angleBetween, degreesToRadians, rotatePoint } from "./engine/math/PointMath";
+import { degreesToRadians, rotatePoint } from "./engine/math/PointMath";
 import {state} from './State'
 import { Explosion } from "./Explosion";
-import { midpointBetween } from "./engine/shapes/Line";
 
 const oneEighty = degreesToRadians(180)
 
@@ -10,17 +9,16 @@ export class Bullet extends GameObject {
 
     constructor(x, y, angle){
         super()
-        //technically I should be translating world coordinates into local coords, but since this objects parent offset will be 0,0 it wont matter
         this.localX = x 
         this.localY = y
         this.distance = 0
-        this.angle = angle
+        this.angle = angle - oneEighty
     }
 
     start(){
         this.maxDistance = 1000
         this.size = 6
-        this.speed = 20
+        this.speed = 5
     }
 
     update(delta){
@@ -36,12 +34,10 @@ export class Bullet extends GameObject {
             return
         }
 
-        // let [midX, midY] = midpointBetween(this.previousX, this.previousY, this.localX, this.localY)
-
-        // if(state.map.tileFromScreenSpace(midX, midY) !== 0 || state.map.tileFromScreenSpace(x, y) !== 0){
-        //     this.parent.spawn(new Explosion(x, y))
-        //     this.destroy()
-        // }   
+        if(state.map.tileFromScreenSpace(this.offsetX, this.offsetY) !== 0){
+            this.parent.spawn(new Explosion(x, y))
+            this.destroy()
+        }   
     }
 
     render(graphics){
