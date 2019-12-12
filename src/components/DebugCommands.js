f09ec508b7321cebadb3625e7602da685550a59b
Joe Johnston <pair+joe.a.johnston@gmail.com>
2019-12-12 14:17:54 -0500

    starting on the magage game UI logic

--- src/components/DebugCommands.js
+++ src/components/DebugCommands.js
@@ -1,13 +1,25 @@
 import React, {Component} from "react"
 import './styles/DebugCommands.scss'
+
+const DEBUG_STATE = {
+    NONE: 'NONE',
+    MANAGE_GAME: 'MANAGE_GAME'
+}
+
+const debugCommandMap = {
+    'manage game': DEBUG_STATE.MANAGE_GAME
+}
+
 class DebugCommands extends Component {
 
     constructor(){
         super()
         this.state = {
-            showDebugConsole: false
+            showDebugConsole: false,
+            debugState: DEBUG_STATE.NONE
         }
         this.toggleCommandPrompt = this.toggleCommandPrompt.bind(this);
+        this.onEnterPressed = this.onEnterPressed.bind(this);
     }
 
     componentDidMount(){
@@ -22,12 +34,25 @@ class DebugCommands extends Component {
         this.setState({showDebugConsole: ~this.state.showDebugConsole})
     }
 
+    onEnterPressed(event){
+        if(event.keyCode === 13){
+
+        }
+    }
+
+    parseCommand(commandString){
+        const commnadState = debugCommandMap[commandString]
+        if(commnadState){
+            this.setState({debugState: commnadState})
+        }
+    }
+
     render(){
         let {} = this.props;
         return(
             this.state.showDebugConsole &&
             <div className='debug-console'>
-                <input type='text' />
+                <input type='text' onKeyDown={this.onEnterPressed}/>
             </div>
         )
     }
