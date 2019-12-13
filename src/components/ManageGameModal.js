fc9f925752eb1707e5a515abe1032eb7e01c2119
Joe Johnston <pair+joe.a.johnston@gmail.com>
2019-12-13 11:41:03 -0500

    started on the game mgmt modal

---
+++ src/components/ManageGameModal.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react"
+
+class ManageGameModal extends Component {
+    render(){
+        let {} = this.props;
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default ManageGameModal;
